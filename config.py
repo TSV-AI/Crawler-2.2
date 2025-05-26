58c061e255998cff964cbc2d078a7357594e8452
TSV-AI <info@threesixtyvue.com>
2025-05-25 21:59:08 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # config.py
 
-BASE_URL = "https://www.theknot.com/marketplace/wedding-reception-venues-atlanta-ga"
+BASE_URL = "https://www.theknot.com/marketplace/wedding-reception-venues-san-diego-ca"
 CSS_SELECTOR = "[class^='info-container']"
 REQUIRED_KEYS = [
     "name",
